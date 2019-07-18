14f89aeb36193181d48a7bfba683f340bfb924b7
Zoltán Vörös <zvoros@gmail.com>
2019-07-18 21:11:04 +0200

    started with profiling section

---
+++ consumeiterable/consumeiterable.c
@@ -0,0 +1,28 @@
+#include "py/obj.h"
+#include "py/runtime.h"
+
+STATIC mp_obj_t consumeiterable_sumsq(mp_obj_t o_in) {
+	mp_float_t _sum = 0.0, itemf;
+	mp_obj_iter_buf_t iter_buf;
+	mp_obj_t item, iterable = mp_getiter(o_in, &iter_buf);
+	while ((item = mp_iternext(iterable)) != MP_OBJ_STOP_ITERATION) {
+		itemf = mp_obj_get_float(item);
+		_sum += itemf*itemf;
+	}
+	return mp_obj_new_float(_sum);
+}
+
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(consumeiterable_sumsq_obj, consumeiterable_sumsq);
+
+STATIC const mp_rom_map_elem_t consumeiterable_module_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_consumeiterable) },
+    { MP_ROM_QSTR(MP_QSTR_sumsq), MP_ROM_PTR(&consumeiterable_sumsq_obj) },
+};
+STATIC MP_DEFINE_CONST_DICT(consumeiterable_module_globals, consumeiterable_module_globals_table);
+
+const mp_obj_module_t consumeiterable_user_cmodule = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t*)&consumeiterable_module_globals,
+};
+
+MP_REGISTER_MODULE(MP_QSTR_consumeiterable, consumeiterable_user_cmodule, MODULE_CONSUMEITERABLE_ENABLED);
---
+++ returniterable/returniterable.c
@@ -0,0 +1,28 @@
+#include "py/obj.h"
+#include "py/runtime.h"
+
+STATIC mp_obj_t powers_iterable(mp_obj_t base, mp_obj_t exponent) {
+	int e = mp_obj_get_int(exponent);
+	mp_obj_t tuple[e+1];
+	int b = mp_obj_get_int(base), ba = 1;
+	for(int i=0; i <= e; i++) {
+		tuple[i] = mp_obj_new_int(ba);
+		ba *= b;
+	}
+	return mp_obj_new_tuple(e+1, tuple);
+}
+
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(powers_iterable_obj, powers_iterable);
+
+STATIC const mp_rom_map_elem_t returniterable_module_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_returniterable) },
+    { MP_ROM_QSTR(MP_QSTR_powers), MP_ROM_PTR(&powers_iterable_obj) },
+};
+STATIC MP_DEFINE_CONST_DICT(returniterable_module_globals, returniterable_module_globals_table);
+
+const mp_obj_module_t returniterable_user_cmodule = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t*)&returniterable_module_globals,
+};
+
+MP_REGISTER_MODULE(MP_QSTR_returniterable, returniterable_user_cmodule, MODULE_RETURNITERABLE_ENABLED);
