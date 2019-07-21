ee388722e5dd52f250b731c06c31496b4479eaa9
Zoltán Vörös <zvoros@gmail.com>
2019-07-21 20:13:20 +0200

    added subscript module

--- profiling/profiling.c
+++ profiling/profiling.c
@@ -5,6 +5,7 @@
 #include "mphalport.h"  // needed for mp_hal_ticks_cpu()
 #include "py/builtin.h" // needed for mp_micropython_mem_info()
 
+/*
 STATIC mp_obj_t measure_cpu(mp_obj_t _x, mp_obj_t _y, mp_obj_t _z) {
 	// first, measure the time taken up by the assignments
 	mp_uint_t start = mp_hal_ticks_cpu();
@@ -14,7 +15,7 @@ STATIC mp_obj_t measure_cpu(mp_obj_t _x, mp_obj_t _y, mp_obj_t _z) {
 	mp_uint_t stop = mp_hal_ticks_cpu();
 	printf("time elapsed: %ld CPU cycles\n", stop - start);
 	mp_micropython_mem_info(0, NULL);
-	
+
 	// measure the time used by the square root
 	start = mp_hal_ticks_cpu();
 	float hypo = sqrtf(x*x + y*y + z*z);
@@ -22,6 +23,26 @@ STATIC mp_obj_t measure_cpu(mp_obj_t _x, mp_obj_t _y, mp_obj_t _z) {
 	printf("time elapsed: %ld CPU cycles\n", stop - start);	
 	mp_micropython_mem_info(0, NULL);
 	return mp_obj_new_float(hypo);
+}*/
+
+
+STATIC mp_obj_t measure_cpu(mp_obj_t _x, mp_obj_t _y, mp_obj_t _z) {
+    size_t start, middle, end;
+    start = m_get_current_bytes_allocated();
+
+    float x = mp_obj_get_float(_x);
+    float y = mp_obj_get_float(_y);
+    float z = mp_obj_get_float(_z);
+    middle = m_get_current_bytes_allocated();
+
+    float hypo = sqrtf(x*x + y*y + z*z);
+    end = m_get_current_bytes_allocated();
+    mp_obj_t tuple[4];
+    tuple[0] = MP_OBJ_NEW_SMALL_INT(start);
+    tuple[1] = MP_OBJ_NEW_SMALL_INT(middle);
+    tuple[2] = MP_OBJ_NEW_SMALL_INT(end);
+    tuple[3] = mp_obj_new_float(hypo);
+    return mp_obj_new_tuple(4, tuple);
 }
 
 STATIC MP_DEFINE_CONST_FUN_OBJ_3(measure_cpu_obj, measure_cpu);
---
+++ subscriptiterable/subscriptiterable.c
@@ -0,0 +1,108 @@
+#include <stdlib.h>
+#include "py/obj.h"
+#include "py/runtime.h"
+
+typedef struct _subitarray_obj_t {
+    mp_obj_base_t base;
+    mp_fun_1_t iternext;
+    uint16_t *elements;
+    size_t len;
+} subitarray_obj_t;
+
+const mp_obj_type_t subiterable_array_type;
+mp_obj_t mp_obj_new_subitarray_iterator(mp_obj_t , size_t , mp_obj_iter_buf_t *);
+
+STATIC void subitarray_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind) {
+	(void)kind;
+	subitarray_obj_t *self = MP_OBJ_TO_PTR(self_in);
+	printf("subitarray: ");
+	for(uint16_t i=0; i < self->len; i++) {
+		printf("%d ", self->elements[i]);
+	}
+	printf("\n");
+}
+
+STATIC mp_obj_t subitarray_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
+	mp_arg_check_num(n_args, n_kw, 1, 1, true);
+	subitarray_obj_t *self = m_new_obj(subitarray_obj_t);
+	self->base.type = &subiterable_array_type;
+	self->len = mp_obj_get_int(args[0]);
+	uint16_t *arr = malloc(self->len * sizeof(uint16_t));
+	for(uint16_t i=0; i < self->len; i++) {
+		arr[i] = i*i;
+	}
+	self->elements = arr;
+	return MP_OBJ_FROM_PTR(self);
+}
+
+STATIC mp_obj_t subitarray_getiter(mp_obj_t o_in, mp_obj_iter_buf_t *iter_buf) {
+    return mp_obj_new_subitarray_iterator(o_in, 0, iter_buf);
+}
+
+STATIC mp_obj_t subitarray_subscr(mp_obj_t self_in, mp_obj_t index, mp_obj_t value) {
+	subitarray_obj_t *self = MP_OBJ_TO_PTR(self_in);
+	size_t idx = mp_obj_get_int(index);
+	printf("%lu, %lu", idx, self->len);
+    if (value == MP_OBJ_NULL) { // simply return the value at index, no assignment 
+		uint16_t *arr = self->elements;
+		printf("%d", arr[3]);
+		return MP_OBJ_NEW_SMALL_INT(arr[idx]);
+	} else { // value was passed, replace the element at index
+		self->elements[idx] = mp_obj_get_int(value);
+	}
+	return mp_const_none;
+}
+
+const mp_obj_type_t subiterable_array_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_subitarray,
+    .print = subitarray_print,
+    .make_new = subitarray_make_new,
+    .getiter = subitarray_getiter,
+    .subscr = subitarray_subscr,
+};
+
+STATIC const mp_rom_map_elem_t subscriptiterable_module_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_subscriptiterable) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_square), (mp_obj_t)&subiterable_array_type },
+};
+STATIC MP_DEFINE_CONST_DICT(subscriptiterable_module_globals, subscriptiterable_module_globals_table);
+
+const mp_obj_module_t subscriptiterable_user_cmodule = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t*)&subscriptiterable_module_globals,
+};
+
+MP_REGISTER_MODULE(MP_QSTR_subscriptiterable, subscriptiterable_user_cmodule, MODULE_SUBSCRIPTITERABLE_ENABLED);
+
+// itarray iterator
+typedef struct _mp_obj_subitarray_it_t {
+    mp_obj_base_t base;
+    mp_fun_1_t iternext;
+    mp_obj_t subitarray;
+    size_t cur;
+} mp_obj_subitarray_it_t;
+
+mp_obj_t subitarray_iternext(mp_obj_t self_in) {
+    mp_obj_subitarray_it_t *self = MP_OBJ_TO_PTR(self_in);
+    subitarray_obj_t *subitarray = MP_OBJ_TO_PTR(self->subitarray);
+    if (self->cur < subitarray->len) {
+		// read the current value
+		uint16_t *arr = subitarray->elements;
+        mp_obj_t o_out = MP_OBJ_NEW_SMALL_INT(arr[self->cur]);
+        self->cur += 1;
+        return o_out;
+    } else {
+        return MP_OBJ_STOP_ITERATION;
+    }
+}
+
+mp_obj_t mp_obj_new_subitarray_iterator(mp_obj_t subitarray, size_t cur, mp_obj_iter_buf_t *iter_buf) {
+    assert(sizeof(mp_obj_subitarray_it_t) <= sizeof(mp_obj_iter_buf_t));
+    mp_obj_subitarray_it_t *o = (mp_obj_subitarray_it_t*)iter_buf;
+    o->base.type = &mp_type_polymorph_iter;
+    o->iternext = subitarray_iternext;
+    o->subitarray = subitarray;
+    o->cur = cur;
+    return MP_OBJ_FROM_PTR(o);
+}
